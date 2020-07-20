4985e9ffc9e27134d52cb012c7b72352f171e440
EVA_1 <rouninlopez@gmail.com>
2020-07-19 22:51:30 -0500

    commiting after archiving and deploying app version

--- ETech.Apps.FridgeIt/FridgeIt/FridgeIt.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ ETech.Apps.FridgeIt/FridgeIt/FridgeIt.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"7615d0a5-da18-4e44-9d75-aaaf9cedfb19"
+	.asciz	"7d9133f8-12d7-4771-9b3a-edc3aba7488b"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
